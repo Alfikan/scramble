03ceb3bece10f296112073d5b566485577b06169
192.168.911.21 <102848825+unkown812@users.noreply.github.com>
2026-02-09 11:16:14 +0000

    Electron removed

--- electron/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.unkown812.scramble',
-  appName: 'Scramble',
-  webDir: 'build'
-};
-
-export default config;
